e50879a1d19439dacae9c348cc24465815f612e9
yasmin belci <yasmin.belci13@gmail.com>
2022-04-29 21:55:24 -0300

    finaliza exercicio

--- Program.cs
+++ Program.cs
@@ -16,15 +16,43 @@ opcaoDesejada = Console.ReadLine()!;
 switch (opcaoDesejada)
 {
     case "+":
+        double w, y, resultado;
         Console.WriteLine("Você selecionou soma");
+        Console.WriteLine("Digite um número: ");
+ Console.Write("w = ");
+        w = Convert.ToDouble(Console.ReadLine());
+        Console.Write("y = ");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        resultado = w + y;
+
+        Console.WriteLine($"{w} somado por {y} é {resultado}");
+
+        Console.WriteLine("\nPressione uma tecla para continuar...");
+        Console.ReadKey();
+
         break;
+   
     case "-":
+        double m, n, solução;
         Console.WriteLine("Você selecionou subtração");
+         Console.WriteLine("Digite um número: ");
+        Console.Write("m = ");
+        m = Convert.ToDouble(Console.ReadLine());
+        Console.Write("n = ");
+        n = Convert.ToDouble(Console.ReadLine());
+
+        solução = m - n;
+
+        Console.WriteLine($"{m} subtraído por {n} é {solução}");
+
+        Console.WriteLine("\nPressione uma tecla para continuar...");
+        Console.ReadKey();
         break;
     case "*":
-        double a, b, resultado;
+        double a, b, conclusão;
 
-        Console.WriteLine("--- Multiplicação A*B ---\n");
+        Console.WriteLine("--- Multiplicação a*b ---\n");
         Console.WriteLine("Digite os valores.");
 
         Console.Write("A = ");
@@ -32,16 +60,30 @@ switch (opcaoDesejada)
         Console.Write("B = ");
         b = Convert.ToDouble(Console.ReadLine());
 
-        resultado = a * b;
+        conclusão = a * b;
 
-        Console.WriteLine($"{a} multiplicado por {b} é {resultado}");
+        Console.WriteLine($"{a} multiplicado por {b} é {conclusão}");
 
         Console.WriteLine("\nPressione uma tecla para continuar...");
         Console.ReadKey();
 
         break;
     case "/":
+        double j, d, resolução;
         Console.WriteLine("Você selecionou divisão");
+         Console.WriteLine("Digite os valores.");
+
+        Console.Write("j = ");
+        j = Convert.ToDouble(Console.ReadLine());
+        Console.Write("d = ");
+        d = Convert.ToDouble(Console.ReadLine());
+
+        resolução = j / d;
+
+        Console.WriteLine($"{j} dividido por {d} é {resolução}");
+
+        Console.WriteLine("\nPressione uma tecla para continuar...");
+        Console.ReadKey();
         break;
     default:
         Console.WriteLine("Opção inválida.");
